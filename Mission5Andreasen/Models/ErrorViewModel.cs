f09aa3f27e428d172c19ba61d6a5137dd09c0962
pla54 <113071547+pla54@users.noreply.github.com>
2024-02-18 23:51:11 -0700

    Add project files.

---
+++ Mission5Andreasen/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mission5Andreasen.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Mission5Andreasen/wwwroot/js/site.js
@@ -0,0 +1,24 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+$(document).ready(function () {
+    $("#calculateButton").click(function () {
+        // Get the number of hours entered by the user
+        var hours = parseFloat($("#hours").val());
+
+        // Check if the input is correct and display if not
+        if (isNaN(hours) || hours < 0) {
+            $("#hoursError").text("Please enter a valid positive number.");
+            return;
+        } else {
+            $("#hoursError").text("");
+        }
+
+        var hourlyRate = 20;
+
+        var totalCost = hours * hourlyRate;
+
+        // Display the total cost
+        $("#totalCost").val("$" + totalCost.toFixed(2));
+    });
+});
